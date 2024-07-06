68efdebc7ec7b4f4e703ae55b51f318edc004cb1
Your Name <jenilpandya911@gmail.com>
2024-07-06 19:41:57 -0400

    Junk

--- 2470-removing-stars-from-a-string/removing-stars-from-a-string.py
+++
@@ -1,9 +0,0 @@
-class Solution(object) :
-    def removeStars(self, s) :
-        res = []
-        for c in s :
-            if res and c == '*':
-                res.pop()
-            else:
-                res.append(c)
-        return ''.join(res)
