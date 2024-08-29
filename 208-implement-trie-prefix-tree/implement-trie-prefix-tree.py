9289aa18f97c5faeb313c971f2020b5c69bcd9b8
Jenil Sanjaykumar Pandya <jenilpandya911@gmail.com>
2024-08-29 16:39:30 -0500

    Time: 102 ms (94.99%) | Memory: 29.4 MB (83.57%) - LeetSync

---
+++ 208-implement-trie-prefix-tree/implement-trie-prefix-tree.py
@@ -0,0 +1,35 @@
+class Trie:
+
+    def __init__(self):
+        self.root={}
+        
+    def insert(self, word: str) -> None:
+
+        cur=self.root
+
+        for letter in word:
+            if letter not in cur:
+                cur[letter]={}
+            cur=cur[letter]
+
+        cur['*']=''
+
+    def search(self, word: str) -> bool:
+
+        cur=self.root
+        for letter in word:
+            if letter not in cur:
+                return False
+            cur=cur[letter]
+
+        return '*' in cur
+        
+    def startsWith(self, prefix: str) -> bool:
+
+        cur=self.root
+        for letter in prefix:
+            if letter not in cur:
+                return False
+            cur=cur[letter]
+
+        return True
